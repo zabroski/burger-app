6789c10dd30e7c49fc5785ccd47c1e839ada488f
Issouf Zabre <sheridanzabre@gmail.com>
2021-03-01 14:08:22 -0500

    calculed ingredients sum dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
+
+    let transformedIngredients = Object.keys(props.ingredients).map(igkey => {
             return [...Array(props.ingredients[igkey])].map((_, i) => {
                return <BurgerIngredient key={igkey + i} type={igkey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
         
     return(
         <div className={classes.Burger}>
