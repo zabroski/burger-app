82105243151731131738c4a56cbc396941d16b7a
Issouf Zabre <sheridanzabre@gmail.com>
2021-03-08 14:00:58 -0500

    created input component wit css looking sexy hahaha

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-order';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state ={
@@ -47,10 +48,10 @@ class ContactData extends Component {
     render () {
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="your Name" />
-                <input className={classes.Input}   type="text" name="name" placeholder="your Mail" />
-                <input className={classes.Input}  type="text" name="name" placeholder="Street" />
-                <input className={classes.Input}  type="text" name="postal" placeholder="Postal" />
+                <Input inputtype="input" type="text" name="name" placeholder="your Name" />
+                <Input inputtype="input" type="text" name="name" placeholder="your Mail" />
+                <Input inputtype="input" type="text" name="name" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
         );
